489236b401bfa58ea9c99c9e960ed65e301f6ad2
jaeyong136 <56102421+jaeyong136@users.noreply.github.com>
2019-11-23 16:03:29 +0900

    readme update

---
+++ module/utils/responseMessage.js
@@ -0,0 +1,9 @@
+module.exports = {
+    ARTICLE_READ_SUCCESS:"게시글 조회 성공", 
+    ARTICLE_READ_FAIL:"게시글 조회 실패", 
+
+    PICTURE_READ_SUCCESS:"사진 조회 성공",
+    PICTURE_READ_FAIL:"사진 조회 실패",
+
+    INTERNAL_SERVER_ERROR: "서버 내부 오류"
+}
