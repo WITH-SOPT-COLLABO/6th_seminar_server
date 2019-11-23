b749028c9399ee60c65ac1fd69e67f67854bca94
jaeyong136 <56102421+jaeyong136@users.noreply.github.com>
2019-11-23 16:40:59 +0900

    update

--- routes/home/bands.js
+++ routes/home/bands.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+const Band = require('../../models/bands');
 
 router.get('/', (req, res) => {
-    
+    Band.read().then(({code, json}) => {
+        res.status(code).send(json);
+    })
+    .catch((err) => {
+        console.log(`err: ${err}`);
+        res.status(statusCode.INTERNAL_SERVER_ERROR).send(authUtil.successFalse(responseMessage.INTERNAL_SERVER_ERROR));
+    });
 })
 
 module.exports = router;
--- routes/home/index.js
+++ routes/home/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 // router.get('/', BlogController.readAll);
-router.use('/articles', require('./articled'));
+router.use('/articles', require('./articles'));
 // router.use('/articles', require('./articles'));
 router.use('/bands', require('./bands'));
 
