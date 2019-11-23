12218f03cf1fbf311053a6abe13c3ac5abce92a2
gngsn <rudtjs4540@gmail.com>
2019-11-23 15:30:18 +0900

    create routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.use('/home', require('./home'));
+router.use('/user', require('./user'));
+router.use('/mypage', require('./mypage'));
+router.use('/signup', require('./signup'));
 
 module.exports = router;
---
+++ routes/mypage/index.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+// router.get('/', BlogController.readAll);
+
+// router.use('/articles', require('./articles'));
+
+module.exports = router;
